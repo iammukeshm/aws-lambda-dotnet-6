546df20d9ed026e8ebdce21aa51e7c51ae6d796f
Mukesh Murugan <iammukeshm@gmail.com>
2022-04-14 20:50:44 +0530

    simple changes

--- HelloLambda/Function.cs
+++ HelloLambda/Function.cs
@@ -14,8 +14,24 @@ public class Function
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public CreateProductResponse FunctionHandler(CreateProductRequest input, ILambdaContext context)
     {
-        return input.ToUpper();
+        var response = new CreateProductResponse();
+        response.ProductId = Guid.NewGuid().ToString();
+        response.Name = input.Name;
+        response.Description = input.Description;
+        return response;
+    }
+
+    public class CreateProductRequest
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+    public class CreateProductResponse
+    {
+        public string? ProductId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
     }
 }
